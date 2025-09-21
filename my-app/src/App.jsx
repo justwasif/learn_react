30307028c4862aa71b8932638b0a32f38835f68f
justwasif <wasif.iit.ansari@gmail.com>
2025-09-21 13:42:56 +0530

    tailwind installed

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-blue-600">
+        Tailwind v3.4.17 is working! 
+      </h1>
+    </>
+  )
+}
+
+export default App
+
