18480828ecb91bce6e09a030ed600f97d0f09351
justwasif <wasif.iit.ansari@gmail.com>
2025-09-21 19:02:31 +0530

    feat : tailwind styling

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './component/Card'
 
-function App() {
+
+
+export default function Card10() {
+  const [count,setcount]=useState(0)
+  let myobj={username:"me",age:18}
   return (
     <>
-      <h1 className="bg-green-400 text-blue-600">
-        Tailwind v3.4.17 is working! 
-      </h1>
+    <h1 className='bg-green-400'>tailwind</h1>
+    <Card username="10" pri="amount"/>
+    <Card username="100" channel="ye" info={myobj}/>
     </>
-  )
+  );
 }
 
-export default App
 
