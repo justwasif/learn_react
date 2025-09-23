7e18b9df4ff68226c80cd77631c44f56c346df8d
justwasif <wasif.iit.ansari@gmail.com>
2025-09-23 19:18:14 +0530

    FEAT:PASSWORD GENERATOR COMP

--- random_password_generator/src/App.jsx
+++ random_password_generator/src/App.jsx
@@ -21,7 +21,19 @@ function App() {
     pass=str.charAt(char)
   }
   return (
-    <h1 className='text-white text-4xl text-center'>Password generator</h1>
+    <div className='w-full max-w-md mx-auto bg-gray-700 text-orange-400 text-4xl text-center'>
+      <h1 className='text-white text-center'>password generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text" value={Password} className='outline-non w-full py-1 px-3' placeholder='password'readOnly></input>
+      <button className='bg-red-700'>copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="range" min={6} max={100} value={lenght} className='cursor-pointer' onChange={(e)=>{setlength(e.target.value)}} />
+          <label>lenght:{lenght}</label>
+        </div>
+      </div>
+    </div>
   
   )
 }
